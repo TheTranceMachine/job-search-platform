f5310f1b7d24e443ecf51841d5f4c05b247046cb
TheTranceMachine <grzegorz.smolin2@gmail.com>
2024-03-01 10:00:35 -0500

    Forms validation

---
+++ src/components/common/validators.js
@@ -0,0 +1,11 @@
+const emailValid = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+};
+
+const passValid = (password) => {
+    const passRegex = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/gm;
+    return passRegex.test(password);
+};
+
+export { emailValid, passValid }
