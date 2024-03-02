c0b4b1345f3960505ee9b4f076053a0df91b148e
TheTranceMachine <grzegorz.smolin2@gmail.com>
2024-03-01 23:00:35 -0500

    ToDoList component

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,8 +1,19 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { TrashIcon } from '@radix-ui/react-icons';
 
 function ToDoList() {
+  const allToDos = useSelector((state) => state.todos.todos);
   const [toDoList, setToDoList] = useState([]);
+
+  const errors = useSelector((state) => state.todos.todos);
+  const allToDosLoading = useSelector((state) => state.todos.todos);
+
+  console.log(allToDos);
+  console.log(allToDosLoading);
+
   const titleRef = useRef('');
+  const dispatch = useDispatch();
 
   const addToDo = () => {
     const title = titleRef.current.value;
@@ -15,19 +26,40 @@ function ToDoList() {
     setToDoList(filteredList);
   };
 
+  useEffect(() => {
+    dispatch({ type: 'TODO_ALL_REQUESTED' });
+  }, []);
+
+  useEffect(() => {
+    setToDoList(allToDos);
+  }, [allToDos]);
+
   return (
     <>
       <div>
         <input type="text" placeholder="Add your task" ref={titleRef} />
-        <div>
-          <button onClick={addToDo}>Submit</button>
+        <div className="my-2">
+          <button
+            onClick={addToDo}
+            className="bg-indigo-500 shadow-lg shadow-indigo-500/50 rounded p-2 w-full text-white"
+          >
+            Submit
+          </button>
         </div>
       </div>
-      <ul>
+      <ul className="my-8">
         {toDoList.map((item) => (
-          <li key={item.id}>
+          <li
+            key={item.id}
+            className="my-2 bg-indigo-300/50 p-2 shadow-sm shadow-indigo-400/50 rounded border border-indigo-300/30 flex justify-between text-white"
+          >
             <span>{item.title}</span>
-            <button onClick={() => removeToDo(item.id)}>Delete</button>
+            <button
+              className="bg-red-500 shadow-sm shadow-indigo-800/50 text-red-800 text-xs p-1 ml-2 rounded"
+              onClick={() => removeToDo(item.id)}
+            >
+              <TrashIcon />
+            </button>
           </li>
         ))}
       </ul>
